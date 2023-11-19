020fdf950242ff8c056525c8467bc414a598742e
mariannam0504 <marianna.melendez@correo.unimet.edu.ve>
2023-11-19 00:12:40 -0400

    tentative view of history table

--- apps/client/components/solutionHistory/SolutionHistoryPage.tsx
+++ apps/client/components/solutionHistory/SolutionHistoryPage.tsx
@@ -1,8 +1,6 @@
 import { useQuery } from '@apollo/client';
 import React from 'react';
-import { Button } from '@avila-tek/ui';
 import { useRouter } from 'next/router';
-// import { createColumnHelper } from '@tanstack/react-table';
 import { GET_MATH_MODELS } from '../../graphql/queries';
 import { useUser } from '../../hooks';
 
@@ -19,36 +17,6 @@ export default function SolutionHistoryPage() {
   });
   const models = data?.mathModels;
 
-  // const columnHelper = createColumnHelper<Record>();
-
-  // const columns = [
-  //   columnHelper.accessor('name', {
-  //     header: 'Lote',
-  //   }),
-  //   // En children deberia ir:
-  //   // children.find((hijos) => hijos.name === 'daysBorn').map((hijo) => hijo.realValue)
-  //   {
-  //     header: 'Días nacidas',
-  //     accessorFn: (row) =>
-  //       row.children.find((hijos) => hijos.name === 'daysBorn')?.realValue,
-  //   },
-  //   {
-  //     header: '# Aves',
-  //     accessorFn: (row) =>
-  //       row.children.find((hijos) => hijos.name === 'totalBirds')?.realValue,
-  //   },
-  //   {
-  //     header: '% Mortalidad',
-  //     accessorFn: (row) =>
-  //       row.children.find((hijos) => hijos.name === 'mortalityRate')?.realValue,
-  //   },
-  //   {
-  //     header: 'Peso promedio (gr) ',
-  //     accessorFn: (row) =>
-  //       row.children.find((hijos) => hijos.name === 'averageWeight')?.realValue,
-  //   },
-  // ];
-
   const onClick = (ModelId) => {
     router.push({
       pathname: '/results',
@@ -57,27 +25,47 @@ export default function SolutionHistoryPage() {
   };
 
   return (
-    <main className="pt-16 px-8 md:px-0 bg-white md:min-h-screen relative flex flex-col space-y-3 md:space-y-20 items-center text-center text-text bg-[url('/img/background-design.jpg')] bg-contain md:bg-auto bg-no-repeat bg-left-bottom">
+    <main className="pt-16 px-8 md:px-0 bg-white md:min-h-screen relative flex flex-col space-y-3 md:space-y-6 items-center text-center text-text bg-[url('/img/background-design.jpg')] bg-contain md:bg-auto bg-no-repeat bg-left-bottom">
       <h1 className="w-10/12 pb-4 border-b border-primary-300 text-3xl md:text-4xl font-bold ">
         Historial de Soluciones
       </h1>
-      <p>Cantidad de modelos generados: {models?.length}</p>
-      <div className="flex flex-wrap gap-4 items-center justify-center">
-        {models?.map((item) => (
-          <div key={item.id}>
-            <Button
-              className="px-6 py-3 flex flex-col items-center justify-center "
-              onClick={() => onClick(item._id)}
-            >
-              <span className="font-semibold">Id del Modelo: </span> {item._id}
-              <span className="font-semibold">
-                {' '}
-                Cantidad de soluciones
-              </span>{' '}
-              {item.solutions.length}
-            </Button>
-          </div>
-        ))}
+      <p className="text-lg text-text-light">
+        Cantidad de modelos generados:{' '}
+        <span className="font-semibold text-text">{models?.length}</span>
+      </p>
+      <div className="w-10/12 p-5 bg-white rounded">
+        <div className="w-full overflow-auto overflow-x-scroll sm:overflow-x-auto text-start">
+          <table className="w-full bg-white rounded text-start">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 border-b text-start">
+                  Nombre del Modelo
+                </th>
+                <th className="py-2 px-4 border-b text-start">Id del Modelo</th>
+                <th className="py-2 px-4 border-b text-start">
+                  Cantidad de Soluciones
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {models?.map((item) => (
+                <React.Fragment key={item.id}>
+                  <tr
+                    key={item.id}
+                    className="hover:bg-gray-100 hover:cursor-pointer"
+                    onClick={() => onClick(item._id)}
+                  >
+                    <td className="py-2 px-4 border-b">{item.name}</td>
+                    <td className="py-2 px-4 border-b">{item._id}</td>
+                    <td className="py-2 px-4 border-b">
+                      {item.solutions.length}
+                    </td>
+                  </tr>
+                </React.Fragment>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </main>
   );
