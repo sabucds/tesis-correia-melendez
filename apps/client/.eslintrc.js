c2eb604a9a9af9f1c1fa9d2a67c94dcc9a14635a
SABRINA CORREIA <sabrina.correia@correo.unimet.edu.ve>
2023-12-07 21:23:03 -0400

    fix

--- apps/client/.eslintrc.js
+++ apps/client/.eslintrc.js
@@ -4,7 +4,10 @@ module.exports = {
   settings: {
     'import/resolver': {
       alias: {
-        map: [['@avila-tek/ui/src', './packages/ui/src']],
+        map: [
+          ['@avila-tek/ui', './packages/ui/src'],
+          ['@avila-tek/ui/src', './packages/ui/src'],
+        ],
         extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
       },
     },
