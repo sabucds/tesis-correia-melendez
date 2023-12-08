53d22458fc85a91ae09590cbad0e1568b1da0fa3
SABRINA CORREIA <sabrina.correia@correo.unimet.edu.ve>
2023-12-07 23:09:10 -0400

    x

--- apps/client/hooks/useNotify.ts
+++ apps/client/hooks/useNotify.ts
@@ -2,6 +2,6 @@ import { useContext } from 'react';
 import { ToastContext } from '../context/ToastContext';
 
 export function useNotify(): (content: string, type: string) => void {
-  const toastContext = useContext(ToastContext);
+  const toastContext = useContext(ToastContext) as any;
   return toastContext.notify;
 }
