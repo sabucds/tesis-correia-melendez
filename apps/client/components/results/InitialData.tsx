99f7899aaaea2d4e9eb64276bce1ed2f400f6b21
mariannam0504 <marianna.melendez@correo.unimet.edu.ve>
2024-03-04 20:52:57 -0400

    cargando fix

--- apps/client/components/results/InitialData.tsx
+++ apps/client/components/results/InitialData.tsx
@@ -43,7 +43,23 @@ export default function InitialData() {
   const initialData = jsonData?.mathModel?.data;
 
   if (loading) {
-    return <p className="h-screen font-semibold">Cargando...</p>;
+    return (
+      <p className="h-screen font-semibold">
+        <div
+          className="fixed top-0 left-0 z-50 w-screen h-screen flex items-center justify-center"
+          style={{ background: 'rgba(0, 0, 0, 0.3)' }}
+        >
+          <div className="bg-white border py-3 px-6 rounded-lg flex items-center flex-col">
+            <div className="loader-dots block relative w-20 h-5 mt-2">
+              <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-primary-300" />
+              <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-primary-300" />
+              <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-primary-300" />
+              <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-primary-300" />
+            </div>
+          </div>
+        </div>
+      </p>
+    );
   }
 
   return (
