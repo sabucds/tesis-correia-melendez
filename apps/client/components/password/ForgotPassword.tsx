c056d643067f12e1bdf0927109a726f087715c47
mariannam0504 <marianna.melendez@correo.unimet.edu.ve>
2024-03-11 12:45:45 -0400

    validación de correo y default values

--- apps/client/components/password/ForgotPassword.tsx
+++ apps/client/components/password/ForgotPassword.tsx
@@ -39,7 +39,7 @@ export default function ForgotPassword() {
       const { data } = await resetPassword({
         variables: {
           data: {
-            email: userEmail,
+            email: userEmail.toLowerCase(),
           },
         },
       });
